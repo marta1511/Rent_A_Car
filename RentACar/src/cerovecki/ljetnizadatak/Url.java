743283b41b61ecae46b53618c74f5f8fa493238d
marta1511 <50339660+marta1511@users.noreply.github.com>
2019-08-17 11:36:06 +0200

    Update UrlEra i UrlGithub

--- RentACar/src/cerovecki/ljetnizadatak/Url.java
+++ RentACar/src/cerovecki/ljetnizadatak/Url.java
@@ -1,29 +1,34 @@
 package cerovecki.ljetnizadatak;
 
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.awt.Desktop;
+
+
+import java.net.URI;
+
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+
+
 
 public class Url {
 	public static Connection veza;
 	public static PreparedStatement izraz;
 	
 	public static void urlEra () {
+	try {
+     Desktop.getDesktop().browse(new URI ("https://github.com/marta1511/Rent_A_Car/blob/master/RentACar/era%20dijagram.png"));
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	}
+	
+	public static void urlGitHub () {
 		try {
-            URL url = new URL("https://github.com/marta1511/Rent_A_Car/blob/master/RentACar/era%20dijagram.png");
-            URLConnection u = url.openConnection();
-            DataInputStream dis = new DataInputStream(u.getInputStream());
-          
-            dis.close();
-        } catch (MalformedURLException me) {
-            System.out.println("MalformedURLException: " + me);
-        } catch (IOException ioe) {
-            System.out.println("IOException: " + ioe);
-        }
+			Desktop.getDesktop().browse(new URI ("https://github.com/marta1511/Rent_A_Car/tree/master/RentACar/src/cerovecki/ljetnizadatak"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
